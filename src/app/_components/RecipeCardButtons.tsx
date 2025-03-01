f2afd2d51d4a16a6d8ed088a181c3003bd6dbd74
rajwindersxxx <rajwindersxxx@gmail.com>
2025-03-01 22:39:50 +0530

    Add shopping cart icon to RecipeCardButtons and improve code formatting

--- src/app/_components/RecipeCardButtons.tsx
+++ src/app/_components/RecipeCardButtons.tsx
@@ -1,4 +1,8 @@
-import {  HiOutlineHeart, HiOutlineThumbUp } from "react-icons/hi";
+import {
+  HiOutlineHeart,
+  HiOutlineShoppingCart,
+  HiOutlineThumbUp,
+} from "react-icons/hi";
 import { useRecipeData } from "../context/RecipeDataContext";
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
@@ -6,26 +10,36 @@ interface props {
   recipeId: number;
 }
 function RecipeCardButtons({ recipeId }: props) {
-  const {toggleLike, likedRecipes, toggleSave, savedRecipes } = useRecipeData();
+  const { toggleLike, likedRecipes, toggleSave, savedRecipes } =
+    useRecipeData();
   const session = useSession();
-  const userid = session.data?.user
+  const userid = session.data?.user;
   const [isLiked, setIsLiked] = useState<boolean>(false);
   const [isSaved, setIsSaved] = useState<boolean>(false);
-  useEffect(()=> {
-     setIsLiked(likedRecipes.includes(recipeId))
-     setIsSaved(savedRecipes.includes(recipeId))
-  }, [likedRecipes, recipeId, savedRecipes])
+  useEffect(() => {
+    setIsLiked(likedRecipes.includes(recipeId));
+    setIsSaved(savedRecipes.includes(recipeId));
+  }, [likedRecipes, recipeId, savedRecipes]);
   return (
     <div className="col-start-3 flex justify-end gap-4">
       <button
         onClick={(e) => {
           e.stopPropagation();
-          if(!userid) return 
+        }}
+      >
+        <HiOutlineShoppingCart
+          className={`h-6 w-6 stroke-natural-terracotta transition-all hover:scale-110 hover:fill-natural-terracotta ${isLiked && "fill-natural-terracotta"}`}
+        />
+      </button>
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          if (!userid) return;
           toggleLike(recipeId);
         }}
       >
         <HiOutlineThumbUp
-          className={`h-6 w-6 stroke-natural-terracotta transition-all hover:scale-110 hover:fill-natural-terracotta ${isLiked && 'fill-natural-terracotta'}`}
+          className={`h-6 w-6 stroke-natural-terracotta transition-all hover:scale-110 hover:fill-natural-terracotta ${isLiked && "fill-natural-terracotta"}`}
         />
       </button>
       <button
@@ -35,7 +49,7 @@ function RecipeCardButtons({ recipeId }: props) {
         }}
       >
         <HiOutlineHeart
-          className={`h-6 w-6 stroke-natural-terracotta transition-all hover:scale-110 hover:fill-natural-terracotta ${isSaved && 'fill-natural-terracotta'}`}
+          className={`h-6 w-6 stroke-natural-terracotta transition-all hover:scale-110 hover:fill-natural-terracotta ${isSaved && "fill-natural-terracotta"}`}
         />
       </button>
     </div>
