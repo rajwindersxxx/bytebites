769d87325a1256d5e57744351653f2730403842d
rajwindersxxx <rajwindersxxx@gmail.com>
2025-04-05 22:36:52 +0530

    finalize v-1

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -1,15 +1,11 @@
 import { Metadata } from "next";
 import SignUpForm from "../_components/forms/SignUpForm";
-import { auth } from "../_lib/Auth";
-import { redirect } from "next/navigation";
+
 export const metadata: Metadata = {
   title: "Sign Up",
 };
 export default async function page() {
-  const session = await auth();
-  if (!session?.user) {
-    redirect("/");
-  }
+
   return (
     <div className="flex h-[calc(100vh-8.125rem)] items-center justify-center">
       <SignUpForm />
