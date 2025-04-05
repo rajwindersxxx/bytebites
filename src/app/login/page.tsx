e7f96aa46bb26a58198c1ee2eebf8112feda6510
rajwindersxxx <rajwindersxxx@gmail.com>
2025-04-05 22:35:11 +0530

    wrong route  protected route on login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,11 @@
 import { Metadata } from "next";
 import LoginForm from "../_components/forms/LoginForm";
-import { auth } from "../_lib/Auth";
-import { redirect } from "next/navigation";
+
 
 export const metadata: Metadata = {
   title: "Sign In",
 };
 async function page() {
-    const session = await auth();
-    if (!session?.user) {
-      redirect("/");
-    }
   return (
     <div className=" flex justify-center items-center  h-[calc(100vh-8.125rem)]">
       <LoginForm/>
